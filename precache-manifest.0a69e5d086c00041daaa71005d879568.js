7475b453241daf46836a4e2305045062fd4460bb
Nicholas Roberto <njr444@gmail.com>
2020-02-21 12:30:12 -0800

    Updates

---
+++ precache-manifest.0a69e5d086c00041daaa71005d879568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b0f990946b96e12c1c78d4cde74e7b",
+    "url": "/beer-front-end/index.html"
+  },
+  {
+    "revision": "77188729b378c598f8e6",
+    "url": "/beer-front-end/static/css/main.6bd9fe77.chunk.css"
+  },
+  {
+    "revision": "60f3e54d7c82aea8abf5",
+    "url": "/beer-front-end/static/js/2.d511f42a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/beer-front-end/static/js/2.d511f42a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77188729b378c598f8e6",
+    "url": "/beer-front-end/static/js/main.64fcef9b.chunk.js"
+  },
+  {
+    "revision": "4b528f3d34c643978942",
+    "url": "/beer-front-end/static/js/runtime-main.b1d0c05f.js"
+  }
+]);
